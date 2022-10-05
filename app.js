929e3868e54364b9ec56c06675b0b0bc63df6fe4
michael-bocim <mtbocim@gmail.com>
2022-10-04 19:02:12 -0600

    finished 1-3

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 "use strict";
 console.log("Let's get this party started!");
 
-let GIPHY_PAGE = "http://api.giphy.com/v1/gifs/search";
+const GIPHY_BASE_URL = "http://api.giphy.com/v1";
 //"api_key"
-let GIPHY_KEY = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+const GIPHY_KEY = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
 
 /**  */
 let response;
 async function findSearchedGiphy(e){
     e.preventDefault();
     let searchTerm = $("form input").val();
-    response = await axios.get(GIPHY_PAGE, {params: {q:searchTerm, api_key:GIPHY_KEY}});
+    response = await axios.get(`${GIPHY_BASE_URL}/gifs/search`, {params: {q:searchTerm, api_key:GIPHY_KEY}});
     //console.log(response.data);
     // $("<img class='giphy'></img>")
     //     .attr("src",`${response.data.data[0].embed_url}`)
     //     .append($(".giphyes"));
-    let randomPictureNum = Math.floor(Math.random() * 50);
+    let randomPictureNum = Math.floor(Math.random() * 50); //get length of data
     let url = response.data.data[randomPictureNum].images.original.url;
-    let gif = $(`<img src="${url}"></img>`);
-    gif.src = url;
-    $(".giphyes").append(gif);
+    let $gif = $(`<img src="${url}"></img>`);
+    //gif.src = url;
+    $(".giphyes").append($gif);
 }
 
 $(".searchBtn")
     .on("click", findSearchedGiphy);
+
+$(".eraseBtn").on("click", thanosSnap);
+
+function thanosSnap(){
+    $(".giphyes").empty();
+}
